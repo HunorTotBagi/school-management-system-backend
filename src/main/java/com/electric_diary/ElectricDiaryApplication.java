046219ae453d7e3d317e4531e34c246165aa67d8
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-05-20 19:54:28 +0200

    first commit

---
+++ src/main/java/com/electric_diary/ElectricDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.electric_diary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electric_diary/ElectricDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electric_diary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
