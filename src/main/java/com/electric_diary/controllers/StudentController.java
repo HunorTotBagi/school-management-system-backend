720bb073f5b10033b49b5059379bce3b14d610b7
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-05-21 20:24:44 +0200

    feat: add basic CRUD operations to StudentController

---
+++ src/main/java/com/electric_diary/controllers/StudentController.java
@@ -0,0 +1,45 @@
+package com.electric_diary.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.electric_diary.entities.StudentEntity;
+import com.electric_diary.handlers.StudentHandler;
+
+@RestController
+@RequestMapping(path = "/api/v1/students")
+public class StudentController {
+
+	@Autowired
+	protected StudentHandler studentHandler;
+
+	@RequestMapping(method = RequestMethod.POST)
+	public StudentEntity createStudent(@RequestParam String firstName, @RequestParam String lastName) {
+		return studentHandler.createStudent(firstName, lastName);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public Iterable<StudentEntity> getAllStudents() {
+		return studentHandler.getAllStudents();
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
+	public StudentEntity getStudentById(@PathVariable String id) {
+		return studentHandler.getStudentById(id);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+	public StudentEntity updateStudent(@PathVariable String id, @RequestParam String firstName,
+			@RequestParam String lastName) {
+		return studentHandler.updateStudent(id, firstName, lastName);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+	public StudentEntity deleteStudent(@PathVariable String id) {
+		return studentHandler.deleteStudent(id);
+	}
+}
---
+++ src/main/java/com/electric_diary/entities/StudentEntity.java
@@ -0,0 +1,42 @@
+package com.electric_diary.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class StudentEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	private String firstName;
+	private String lastName;
+
+	public StudentEntity() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
