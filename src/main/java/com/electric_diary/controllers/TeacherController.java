02306200a8c5ffd32ffcf8a6aba5a16eb70d0729
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-05-23 20:03:11 +0200

    add logger instance into TeacherController

--- src/main/java/com/electric_diary/controllers/TeacherController.java
+++ src/main/java/com/electric_diary/controllers/TeacherController.java
@@ -1,7 +1,17 @@
 package com.electric_diary.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.electric_diary.entities.TeacherEntity;
 import com.electric_diary.services.TeacherService;
 
@@ -11,24 +21,30 @@ public class TeacherController {
 
 	@Autowired
 	protected TeacherService teacherService;
+	
+	private final Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
 
 	@PostMapping
 	public TeacherEntity createTeacher(@RequestBody TeacherEntity teacherBody) {
+		logger.debug("This is a debug message");
 		return teacherService.createTeacher(teacherBody);
 	}
 
 	@GetMapping
 	public Iterable<TeacherEntity> getAllTeachers() {
+		logger.info("This is an info message");
 		return teacherService.getAllTeachers();
 	}
 
 	@GetMapping("/{id}")
 	public TeacherEntity getTeacherById(@PathVariable String id) {
+		logger.warn("This is a warn message");
 		return teacherService.getTeacherById(id);
 	}
 
 	@PutMapping("/{id}")
 	public TeacherEntity updateTeacher(@PathVariable String id, @RequestBody TeacherEntity teacherBody) {
+		logger.error("This is an error message");
 		return teacherService.updateTeacher(id, teacherBody);
 	}
 
