4e337bf1cdcd56af1d785375850514771cbd26b0
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-05-23 20:05:26 +0200

    fix: improve imports

--- src/main/java/com/electric_diary/controllers/TeacherController.java
+++ src/main/java/com/electric_diary/controllers/TeacherController.java
@@ -3,15 +3,7 @@ package com.electric_diary.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.electric_diary.entities.TeacherEntity;
 import com.electric_diary.services.TeacherService;
 
