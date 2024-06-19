d488beca85249caf01e01beee81ae943d19df8cc
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-06-19 12:53:37 +0200

    fix: formatting

--- src/main/java/com/electric_diary/entities/ClassEntity.java
+++ src/main/java/com/electric_diary/entities/ClassEntity.java
@@ -11,7 +11,7 @@ public class ClassEntity {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String name;
-	
+
 	public ClassEntity() {
 	}
 
--- src/main/java/com/electric_diary/services/impl/ClassServiceImpl.java
+++ src/main/java/com/electric_diary/services/impl/ClassServiceImpl.java
@@ -16,8 +16,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
 @Service
-public class ClassServiceImpl extends ErrorMessagesServiceImpl implements ClassService  {
-	
+public class ClassServiceImpl extends ErrorMessagesServiceImpl implements ClassService {
+
 	@PersistenceContext
 	protected EntityManager em;
 
