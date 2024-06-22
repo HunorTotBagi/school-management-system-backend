254c9d0d50ea821fc7310bf838f4ac3a466cff73
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-06-22 18:12:55 +0200

    refactor: class controller & service

--- src/main/java/com/electric_diary/services/impl/ClassServiceImpl.java
+++ src/main/java/com/electric_diary/services/impl/ClassServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
 import com.electric_diary.entities.ClassEntity;
+import com.electric_diary.exception.CustomBadRequestException;
+import com.electric_diary.exception.NotFoundException;
 import com.electric_diary.repositories.ClassRepository;
 import com.electric_diary.services.ClassService;
 
@@ -25,10 +27,9 @@ public class ClassServiceImpl extends ErrorMessagesServiceImpl implements ClassS
 	protected ClassRepository classRepository;
 
 	@Override
-	public ResponseEntity<?> createClass(ClassEntity classBody, BindingResult result) {
-		if (result.hasErrors()) {
-			return new ResponseEntity<>(createErrorMessage(result), HttpStatus.BAD_REQUEST);
-		}
+	public ResponseEntity<ClassEntity> createClass(ClassEntity classBody, BindingResult result) {
+		if (result.hasErrors())
+			throw new CustomBadRequestException(result);
 
 		ClassEntity newClass = new ClassEntity();
 		newClass.setName(classBody.getName());
@@ -38,67 +39,59 @@ public class ClassServiceImpl extends ErrorMessagesServiceImpl implements ClassS
 	}
 
 	@Override
-	public ResponseEntity<?> getAllClasses() {
+	public ResponseEntity<Iterable<ClassEntity>> getAllClasses() {
 		Iterable<ClassEntity> newClass = classRepository.findAll();
 		return new ResponseEntity<>(newClass, HttpStatus.OK);
 	}
 
 	@Override
-	public ResponseEntity<?> getClassById(String id) {
+	public ResponseEntity<ClassEntity> getClassById(String id) {
 		try {
 			int classId = Integer.parseInt(id);
-			Optional<ClassEntity> classOptional = classRepository.findById(classId);
-
-			if (classOptional.isPresent()) {
-				return new ResponseEntity<>(classOptional.get(), HttpStatus.OK);
-			} else {
-				return createNotFoundResponse("Class", classId);
-			}
+			ClassEntity classEntity = classRepository.findById(classId)
+					.orElseThrow(() -> new NotFoundException("Class", id));
+			return ResponseEntity.ok(classEntity);
 		} catch (NumberFormatException e) {
-			return createBadRequestResponse("Invalid ID format");
-		} catch (Exception e) {
-			return createErrorResponse(e);
+			throw new NumberFormatException();
 		}
 	}
 
 	@Override
-	public ResponseEntity<?> updateClass(String id, ClassEntity classBody) {
+	public ResponseEntity<ClassEntity> updateClass(String id, ClassEntity classBody) {
+		int classId;
 		try {
-			int classId = Integer.parseInt(id);
-			Optional<ClassEntity> optionalClass = classRepository.findById(classId);
-
-			if (optionalClass.isPresent()) {
-				ClassEntity newClass = optionalClass.get();
-				newClass.setName(classBody.getName());
-				classRepository.save(newClass);
-				return new ResponseEntity<>(newClass, HttpStatus.OK);
-			} else {
-				return createNotFoundResponse("Class", classId);
-			}
+			classId = Integer.parseInt(id);
 		} catch (NumberFormatException e) {
-			return createBadRequestResponse("Invalid ID format");
-		} catch (Exception e) {
-			return createErrorResponse(e);
+			throw new NumberFormatException();
+		}
+
+		Optional<ClassEntity> optionalClass = classRepository.findById(classId);
+		if (optionalClass.isPresent()) {
+			ClassEntity newClass = optionalClass.get();
+			newClass.setName(classBody.getName());
+			classRepository.save(newClass);
+			return new ResponseEntity<>(newClass, HttpStatus.OK);
+		} else {
+			throw new NotFoundException("Class", id);
 		}
 	}
 
 	@Override
-	public ResponseEntity<?> deleteClass(String id) {
+	public ResponseEntity<ClassEntity> deleteClass(String id) {
+		int classId;
 		try {
-			int classId = Integer.parseInt(id);
-			Optional<ClassEntity> optionalClass = classRepository.findById(classId);
-
-			if (optionalClass.isPresent()) {
-				ClassEntity newClass = optionalClass.get();
-				classRepository.delete(newClass);
-				return new ResponseEntity<>(newClass, HttpStatus.OK);
-			} else {
-				return createNotFoundResponse("Class", classId);
-			}
+			classId = Integer.parseInt(id);
 		} catch (NumberFormatException e) {
-			return createBadRequestResponse("Invalid ID format");
-		} catch (Exception e) {
-			return createErrorResponse(e);
+			throw new NumberFormatException();
+		}
+
+		Optional<ClassEntity> optionalClass = classRepository.findById(classId);
+		if (optionalClass.isPresent()) {
+			ClassEntity newClass = optionalClass.get();
+			classRepository.delete(newClass);
+			return new ResponseEntity<>(newClass, HttpStatus.OK);
+		} else {
+			throw new NotFoundException("Class", id);
 		}
 	}
 
