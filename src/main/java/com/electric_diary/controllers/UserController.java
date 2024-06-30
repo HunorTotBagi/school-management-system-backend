eee04461f5df32e74f50832c34e90fd3b3585492
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-06-30 15:51:32 +0200

    wip

--- src/main/java/com/electric_diary/controllers/UserController.java
+++ src/main/java/com/electric_diary/controllers/UserController.java
@@ -1,5 +1,8 @@
 package com.electric_diary.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.electric_diary.entities.RoleEntity;
 import com.electric_diary.entities.UserEntity;
 import com.electric_diary.services.UserService;
 
@@ -21,6 +25,37 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping(path = "/api/v1/users")
 public class UserController {
+
+	public List<UserEntity> getDummyDB() {
+		List<UserEntity> list = new ArrayList<>();
+		RoleEntity re = new RoleEntity();
+
+		re.setId(1);
+		re.setName("admin");
+
+		UserEntity ue = new UserEntity();
+		ue.setId(1);
+		ue.setUsername("hunor");
+		ue.setPassword("password1234");
+		ue.setRole(re);
+
+		UserEntity ue1 = new UserEntity();
+
+		ue1.setId(2);
+		ue1.setUsername("milica");
+		ue1.setPassword("password4321");
+		ue1.setRole(re);
+
+		list.add(ue);
+		list.add(ue1);
+
+		return list;
+	}
+
+	@GetMapping("/database")
+	public ResponseEntity<?> listUsers() {
+		return new ResponseEntity<List<UserEntity>>(getDummyDB(), HttpStatus.OK);
+	}
 
 	@Autowired
 	protected UserService userService;
--- src/main/java/com/electric_diary/entities/UserEntity.java
+++ src/main/java/com/electric_diary/entities/UserEntity.java
@@ -1,21 +1,31 @@
 package com.electric_diary.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
+@Table(name = "user")
+@JsonIgnoreProperties({ "handler", "hibernateLazyInitializer" })
 public class UserEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "user_id")
 	private Integer id;
 
-    @Column(unique = true, nullable = false)
+    @Column(name = "username", unique = true, nullable = false)
 	@NotBlank(message = "Username cannot be blank")
 	@NotNull(message = "Username name must be provided.")
 	@Size(min = 2, max = 30, message = "Username must be between {min} and {max} characters long.")
@@ -25,6 +35,10 @@ public class UserEntity {
 	@NotNull(message = "Password must be provided.")
 	@Size(min = 2, max = 30, message = "Password must be between {min} and {max} characters long.")
 	private String password;
+	
+	@ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+	@JoinColumn(name = "role")
+	private RoleEntity role;
 
 	public UserEntity() {
 	}
@@ -51,5 +65,13 @@ public class UserEntity {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public RoleEntity getRole() {
+		return role;
+	}
+
+	public void setRole(RoleEntity role) {
+		this.role = role;
 	}
 }
