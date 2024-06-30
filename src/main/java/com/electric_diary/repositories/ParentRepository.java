e347f4696d764a5c8dc45662caae522f1680590b
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-06-30 13:24:36 +0200

    feat: sending to students parent email

--- src/main/java/com/electric_diary/repositories/ParentRepository.java
+++ src/main/java/com/electric_diary/repositories/ParentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.electric_diary.entities.ParentEntity;
 
 public interface ParentRepository extends CrudRepository<ParentEntity, Integer> {
-
+	ParentEntity findByStudentsId(Integer studentId);
 }
