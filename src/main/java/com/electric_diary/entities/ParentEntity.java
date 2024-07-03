0b72140b55363d99f70da5d14dd516f670a80083
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-07-04 00:11:35 +0200

    refactor: improve ParentEntity

--- src/main/java/com/electric_diary/entities/ParentEntity.java
+++ src/main/java/com/electric_diary/entities/ParentEntity.java
@@ -5,19 +5,33 @@ import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class ParentEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+
+	@NotBlank(message = "First name must be provided")
+	@Size(min = 2, max = 30, message = "First name must be between {min} and {max} characters long.")
 	private String firstName;
+
+	@NotBlank(message = "Last name must be provided")
+	@Size(min = 2, max = 30, message = "Last name must be between {min} and {max} characters long.")
 	private String lastName;
+
+	@Column(unique = true)
+	@NotBlank(message = "Email is mandatory")
+	@Email(message = "Email should be valid")
 	private String email;
 
 	@JsonIgnore
