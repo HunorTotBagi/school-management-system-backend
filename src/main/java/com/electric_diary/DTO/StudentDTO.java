46c84bbc798416549b0b8121b9ebe5629ebc7cfd
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-08-01 08:14:46 +0200

    refactor: add lombok to dtos

--- src/main/java/com/electric_diary/DTO/StudentDTO.java
+++ src/main/java/com/electric_diary/DTO/StudentDTO.java
@@ -1,43 +1,15 @@
 package com.electric_diary.DTO;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class StudentDTO {
 	private String firstName;
 	private String lastName;
 	private String classId;
 	private String parentId;
-	
-	public StudentDTO() {
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getClassId() {
-		return classId;
-	}
-
-	public void setClassId(String classId) {
-		this.classId = classId;
-	}
-
-	public String getParentId() {
-		return parentId;
-	}
-
-	public void setParentId(String parentId) {
-		this.parentId = parentId;
-	}
 }
--- src/main/java/com/electric_diary/DTO/TeacherDTO.java
+++ src/main/java/com/electric_diary/DTO/TeacherDTO.java
@@ -1,52 +1,16 @@
 package com.electric_diary.DTO;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class TeacherDTO {
-    private String firstName;
-    private String lastName;
-    private String teacherId;
-    private String subjectId;
-    private String classId;
-
-    public TeacherDTO() {
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getTeacherId() {
-        return teacherId;
-    }
-
-    public void setTeacherId(String teacherId) {
-        this.teacherId = teacherId;
-    }
-
-    public String getSubjectId() {
-        return subjectId;
-    }
-
-    public void setSubjectId(String subjectId) {
-        this.subjectId = subjectId;
-    }
-
-    public String getClassId() {
-        return classId;
-    }
-
-    public void setClassId(String classId) {
-        this.classId = classId;
-    }
+	private String firstName;
+	private String lastName;
+	private String teacherId;
+	private String subjectId;
+	private String classId;
 }
