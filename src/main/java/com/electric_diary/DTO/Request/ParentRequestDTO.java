13ec8e455c5c9e8e0fc61a7bc123edd7a4269548
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-08-06 14:54:16 +0200

    chore: add anotations to DTOs

--- src/main/java/com/electric_diary/DTO/Request/ParentRequestDTO.java
+++ src/main/java/com/electric_diary/DTO/Request/ParentRequestDTO.java
@@ -1,5 +1,8 @@
 package com.electric_diary.DTO.Request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,8 +11,16 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class ParentRequestDTO {
+	@NotBlank(message = "First name must be provided")
+	@Size(min = 2, max = 30, message = "First name must be between {min} and {max} characters long.")
 	private String firstName;
+	
+	@NotBlank(message = "Last name must be provided")
+	@Size(min = 2, max = 30, message = "Last name must be between {min} and {max} characters long.")
 	private String lastName;
+	
+	@NotBlank(message = "Email is mandatory")
+	@Email(message = "Email should be valid")
 	private String email;
 	private Integer userId;
 }
