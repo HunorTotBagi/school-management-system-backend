9356cfdf4571fc078ee2738993de1d273a654627
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-09-17 23:15:20 +0200

    feat: add cross origin for port 3000

--- src/main/java/com/electric_diary/controllers/ParentController.java
+++ src/main/java/com/electric_diary/controllers/ParentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(path = "/api/v1/parents")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ParentController {
 	@Autowired
 	protected ParentService parentService;
